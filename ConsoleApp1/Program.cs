13257e921b01d1c89c83413997348acbcc3f32c0
Jiri Cincura <jiri@cincura.net>
2024-10-21 14:28:17 +0200

    Progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,23 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using var db = new DogOwnersClubContext();
 await db.Database.EnsureDeletedAsync();
 await db.Database.EnsureCreatedAsync();
-db.Add(new Person { Name = new FullName("John", "Doe"), Duration = new Duration(1, 2) });
+var p = new Person { Name = new FullName { FirstName = "John", LastName = "Doe", foo = new Foo { Bar = "Test" } }, Duration = new Duration(1, 2) };
+db.Add(p);
+//db.Add(new Dog()
+//{
+//    Owner = p,
+//    DOB = new DateOnly(2021, 1, 1),
+//    ShowResults = [new ShowResult { Rating = 1, Comment = "Good" }],
+//    VetVisits = [new DateOnly(2021, 1, 1), new DateOnly(2021, 1, 1)],
+//});
+//db.SaveChanges();
+//db.Owners.Where(x => x.Name.FirstName.StartsWith("A")).Load();
+//db.Owners.Where(x => x.Duration.Minutes > 10).Load();
+//db.Dogs.Where(x => x.VetVisits.Last() < DateOnly.FromDateTime(DateTime.Now.AddDays(-90))).Load();
+db.Add(new BigDog { DOB = new DateOnly(2021, 1, 1), Weight = 10, Owner = p });
 db.SaveChanges();
+db.Dogs.Where(x => x is SmallDog).Load();
+
 
 class DogOwnersClubContext : DbContext
 {
@@ -22,6 +37,25 @@ class DogOwnersClubContext : DbContext
 
         modelBuilder.ApplyConfiguration(new PersonConfiguration());
         modelBuilder.ApplyConfiguration(new DogConfiguration());
+
+        //modelBuilder.Entity<BigDog>(b =>
+        //{
+        //    b.UseTphMappingStrategy();
+        //    b.HasDiscriminator<string>("D")
+        //        .HasValue<BigDog>("B");
+        //});
+        //modelBuilder.Entity<SmallDog>(b =>
+        //{
+        //    b.UseTphMappingStrategy();
+        //    b.HasDiscriminator<string>("D")
+        //        .HasValue<SmallDog>("S");
+        //});
+        modelBuilder.Entity<BigDog>(b =>
+        {
+        });
+        modelBuilder.Entity<SmallDog>(b =>
+        {
+        });
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -41,12 +75,19 @@ class Person
     public ICollection<Dog> Dogs { get; set; } = [];
     public Duration Duration { get; set; } = null!;
 }
-class Dog
+abstract class Dog
 {
     public int Id { get; set; }
     public DateOnly DOB { get; set; }
     public Person Owner { get; set; } = null!;
-    public List<ShowResult> ShowResults { get; set; } = [];
+}
+class BigDog : Dog
+{
+    public int Weight { get; set; }
+}
+class SmallDog : Dog
+{
+    public float Dummy { get; set; }
 }
 
 class ShowResult
@@ -63,16 +104,25 @@ class PersonConfiguration : IEntityTypeConfiguration<Person>
         builder.HasKey(x => x.Id);
         builder.ToTable("Owners");
         builder.OwnsOne(x => x.Duration).ToJson();
-        builder.ComplexProperty(x => x.Name);
+        builder.ComplexProperty(x => x.Name, x => x.ComplexProperty(y => y.foo));
     }
 }
 class DogConfiguration : IEntityTypeConfiguration<Dog>
 {
     public void Configure(EntityTypeBuilder<Dog> builder)
     {
-        builder.OwnsMany(x => x.ShowResults).ToJson();
+        builder.UseTpcMappingStrategy();
     }
 }
 
 record Duration(int Minutes, int Seconds);
-record FullName(string FirstName, string LastName);
+class FullName
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Foo foo { get; set; }
+}
+class Foo
+{
+    public string Bar { get; set; }
+}
