3001f1e091a1f4e97498b0f5493ce97292edcb43
Jiri Cincura <jiri@cincura.net>
2024-10-21 11:44:18 +0200

    Progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,18 +7,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using var db = new DogOwnersClubContext();
 await db.Database.EnsureDeletedAsync();
 await db.Database.EnsureCreatedAsync();
-//db.Owners.IgnoreQueryFilters().TagWithCallSite().Load();
-//db.Owners.Where(x => x.Duration == new Duration(10)).Load();
-//db.Owners.Where(x => EF.Property<DateTime>(x, "LastUpdated") > DateTime.Now).Load();
-//var o = new Person();
-//db.Add(o);
-//db.Entry(o).Property<DateTime>("LastUpdated").CurrentValue = DateTime.Now;
-db.Set<Dictionary<string, object>>("FooBar").Add(new Dictionary<string, object>
-{
-    ["Id"] = 1,
-    ["Value"] = "Foo",
-    ["Created"] = DateTime.Now
-});
+db.Add(new Person { Name = "John", Duration = new Duration(1, 2, ["aaa", "bbb"]) });
+db.SaveChanges();
+db.Owners.Where(x => x.Duration.Strings[0] == "aaa").Load();
 
 class DogOwnersClubContext : DbContext
 {
@@ -30,24 +21,6 @@ class DogOwnersClubContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfiguration(new PersonConfiguration());
-        modelBuilder.ApplyConfiguration(new CommentConfiguration());
-
-        modelBuilder.SharedTypeEntity<Dictionary<string, object>>("FooBar", b =>
-        {
-            b.IndexerProperty<int>("Id");
-            b.IndexerProperty<string>("Value");
-            b.IndexerProperty<DateTime>("Created");
-        });
-        modelBuilder.SharedTypeEntity<Dictionary<string, object>>("Joe", b =>
-        {
-            b.IndexerProperty<int>("Id");
-            b.IndexerProperty<string>("Value");
-            b.IndexerProperty<DateTime>("Created");
-            b.HasMany("FooBars")
-                .WithOne("Joe")
-                .HasForeignKey("_joeId")
-                .OnDelete(DeleteBehavior.Cascade);
-        });
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -60,42 +33,18 @@ class DogOwnersClubContext : DbContext
     }
 }
 
-//[Table("T_OWNER")]
 class Person
 {
-    //[Key]
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public ICollection<Dog> Dogs { get; set; } = [];
-    private bool _active;
-    internal bool Active => _active;
     public Duration Duration { get; set; } = null!;
-
-    public void Deactivate()
-    {
-        if (!_active)
-            throw new InvalidOperationException("Already deactivated");
-        _active = false;
-    }
 }
 class Dog
 {
     public int Id { get; set; }
     public DateOnly DOB { get; set; }
     public Person Owner { get; set; } = null!;
-    public ICollection<Comment> Comments { get; set; } = [];
-}
-class Comment
-{
-    private Dictionary<string, object> _attributes = [];
-
-    public int Id { get; set; }
-
-    public object this[string name]
-    {
-        get => _attributes[name];
-        set => _attributes[name] = value;
-    }
 }
 
 class PersonConfiguration : IEntityTypeConfiguration<Person>
@@ -107,32 +56,8 @@ class PersonConfiguration : IEntityTypeConfiguration<Person>
             .HasMaxLength(100)
             .IsUnicode();
         builder.ToTable("Owners");
-        builder.HasQueryFilter(x => x.Active);
-        builder.Property(x => x.Duration)
-            .HasConversion(new DurationConverter());
-        builder.Property(x => x.Active)
-            .HasField("_active")
-            .UsePropertyAccessMode(PropertyAccessMode.Field);
-        builder.Property<DateTime>("LastUpdated");
+        builder.OwnsOne(x => x.Duration).ToJson();
     }
 }
 
-class CommentConfiguration : IEntityTypeConfiguration<Comment>
-{
-    public void Configure(EntityTypeBuilder<Comment> builder)
-    {
-        builder.HasKey(x => x.Id);
-        builder.IndexerProperty<DateTime>("Created");
-        builder.IndexerProperty<string>("Author");
-        builder.IndexerProperty<string>("Text");
-    }
-}
-
-record Duration(int Seconds);
-
-class DurationConverter : ValueConverter<Duration, int>
-{
-    public DurationConverter()
-        : base(x => x.Seconds, x => new Duration(x))
-    { }
-}
+record Duration(int Minutes, int Seconds, string[] Strings);
