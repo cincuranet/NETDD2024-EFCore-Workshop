0db84b1a52f4f3af13583b20ca9dcd72d7c49e85
Jiri Cincura <jiri@cincura.net>
2024-10-21 09:24:52 +0200

    Progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+class DogOwnersClub : DbContext
+{
+    public DbSet<Person> Owners { get; set; }
+    public DbSet<Dog> Dogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<Dog> Dogs { get; set; } = [];
+}
+class Dog
+{
+    public int Id { get; set; }
+    public DateOnly DOB { get; set; }
+    public Person Owner { get; set; } = null!;
+}
