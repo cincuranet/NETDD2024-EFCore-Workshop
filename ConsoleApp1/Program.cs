ef619d15b8a7b74773aea4ae3ba9b8554cc87ad2
Jiri Cincura <jiri@cincura.net>
2024-10-21 11:12:27 +0200

    Progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 using var db = new DogOwnersClubContext();
 await db.Database.EnsureDeletedAsync();
 await db.Database.EnsureCreatedAsync();
-
+//db.Owners.IgnoreQueryFilters().TagWithCallSite().Load();
+//db.Owners.Where(x => x.Duration == new Duration(10)).Load();
+//db.Owners.Where(x => EF.Property<DateTime>(x, "LastUpdated") > DateTime.Now).Load();
+//var o = new Person();
+//db.Add(o);
+//db.Entry(o).Property<DateTime>("LastUpdated").CurrentValue = DateTime.Now;
 
 class DogOwnersClubContext : DbContext
 {
@@ -18,6 +24,14 @@ class DogOwnersClubContext : DbContext
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfiguration(new PersonConfiguration());
+        modelBuilder.ApplyConfiguration(new CommentConfiguration());
+
+        modelBuilder.SharedTypeEntity<Dictionary<string, object>>("FooBar", b =>
+        {
+            b.IndexerProperty<int>("Id");
+            b.IndexerProperty<string>("Value");
+            b.IndexerProperty<DateTime>("Created");
+        });
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -37,12 +51,35 @@ class Person
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public ICollection<Dog> Dogs { get; set; } = [];
+    private bool _active;
+    internal bool Active => _active;
+    public Duration Duration { get; set; } = null!;
+
+    public void Deactivate()
+    {
+        if (!_active)
+            throw new InvalidOperationException("Already deactivated");
+        _active = false;
+    }
 }
 class Dog
 {
     public int Id { get; set; }
     public DateOnly DOB { get; set; }
     public Person Owner { get; set; } = null!;
+    public ICollection<Comment> Comments { get; set; } = [];
+}
+class Comment
+{
+    private Dictionary<string, object> _attributes = [];
+
+    public int Id { get; set; }
+
+    public object this[string name]
+    {
+        get => _attributes[name];
+        set => _attributes[name] = value;
+    }
 }
 
 class PersonConfiguration : IEntityTypeConfiguration<Person>
@@ -54,5 +91,32 @@ class PersonConfiguration : IEntityTypeConfiguration<Person>
             .HasMaxLength(100)
             .IsUnicode();
         builder.ToTable("Owners");
+        builder.HasQueryFilter(x => x.Active);
+        builder.Property(x => x.Duration)
+            .HasConversion(new DurationConverter());
+        builder.Property(x => x.Active)
+            .HasField("_active")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+        builder.Property<DateTime>("LastUpdated");
     }
+}
+
+class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.IndexerProperty<DateTime>("Created");
+        builder.IndexerProperty<string>("Author");
+        builder.IndexerProperty<string>("Text");
+    }
+}
+
+record Duration(int Seconds);
+
+class DurationConverter : ValueConverter<Duration, int>
+{
+    public DurationConverter()
+        : base(x => x.Seconds, x => new Duration(x))
+    { }
 }
